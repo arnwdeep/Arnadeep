e335a198bf92eb077a990c660b633bbceb241e28
Arnadeep <modakarnadeep@gmail.com>
2026-02-17 15:29:07 +0530

    UC2

---
+++ OOPSPrintBanner.java
@@ -0,0 +1,15 @@
+public class OOPSPrintBanner {
+
+    public static void main(String[] args) {
+
+        System.out.println(" *****    *****    *****    ***** ");
+        System.out.println("*     *  *     *  *     *  *     *");
+        System.out.println("*     *  *     *  *     *  *     *");
+        System.out.println("*     *  *     *  *****    ***** ");
+        System.out.println("*     *  *     *  *             *");
+        System.out.println("*     *  *     *  *             *");
+        System.out.println(" *****    *****    *        ***** ");
+
+    }
+
+}
